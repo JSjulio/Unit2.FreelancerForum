96fb430322b0bba28f8786d6a22b9ddd3443e804
Isaias Julio <isaiasjulio@Isaiass-MacBook-Air-2.local>
2023-11-20 16:45:47 -0500

    final

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-/* State */
 const freelancers = [];
 const maxFreelancers = 10;
 
-// `setInterval` will call `addFreelancer` every 3000 milliseconds (3 seconds)
 const addFreelancerIntervalId = setInterval(addFreelancer, 3000);
 
-render(); // We call this function once to render the initial state
+render(); 
 
-/**
- * Update the DOM to reflect the current state.
- * The term "render" is often used to describe this process.
- */
+
 function render() {
   // Render the freelancers
   const freelancersList = document.querySelector("#freelancers");
@@ -28,9 +23,7 @@ function render() {
   averageText.textContent = `Average Starting Price: $${averagePrice.toFixed(2)}`;
 }
 
-/**
- * Add a random freelancer to the `freelancers` array
- */
+
 function addFreelancer() {
   const names = ["Alice", "Bob", "Carol", "David", "Eva"];
   const occupations = ["Writer", "Teacher", "Programmer", "Designer", "Translator"];
